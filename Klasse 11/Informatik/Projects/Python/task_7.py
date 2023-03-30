5ffff2f02b186558871e317df3f5c7c79a70de7e
karl-zschiebsch <karl.zschiebsch@gmail.com>
2023-03-30 11:43:10 +0200

    vault backup: 2023-03-30 11:43:10

--- Klasse 11/Informatik/Projects/Python/task_7.py
+++ Klasse 11/Informatik/Projects/Python/task_7.py
@@ -2,10 +2,21 @@ sqares = {
 
 }
 """
-
+Square numbers in [1:10]
 """
-for i in range(1, 10):
+for i in range(1, 11):
     sqares[i] = i ** 2
-sum = 101 * 50
-
 print(sqares)
+
+sum = 101 * 50
+"""
+The sum of [1:100]
+"""
+print(sum)
+
+from math import factorial
+fac = factorial(100)
+"""
+The product of [1:100]
+"""
+print(fac)
